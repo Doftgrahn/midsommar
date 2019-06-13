8bb99a0f76d6bef6a2d868af1ced521fcd5323d1
Simon grahn <simon.grahn@gmail.com>
2019-06-13 14:40:06 +0200

    kjk

--- src/components/LoggedOut.jsx
+++ src/components/LoggedOut.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-
+import React from "react";
 
 const LoggedOut = ({logIn}) => {
-    return (<main className="logLoggedIn">
-        <div className="video">
-            <iframe
-                src="https://player.vimeo.com/video/152962257?autoplay=1&loop=1"
-                frameBorder="0"
-                allow="autoplay; fullscreen"
-                allowFullScreen
-                title="k"
-            />
-        </div>
-        <button onClick={logIn}>Log In!!!!</button>
-    </main>)
-}
-
+    return (
+        <main className="logLoggedIn">
+            <div className="video">
+                <iframe
+                    title="''"
+                    src="https://player.vimeo.com/video/177527141?autoplay=1&loop=1&title=0&byline=0&portrait=0"
+                    width="640"
+                    height="360"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                />
+            </div>
+            <button onClick={logIn}>Log In!!!!</button>
+        </main>
+    );
+};
 
 export default LoggedOut;
